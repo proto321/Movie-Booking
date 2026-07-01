22e499e733daf63b7ce3cbb83aa2ef0e77b4bfdf
Harsh Sahu <prototype.myid@gmail.com>
2026-07-01 15:38:42 +0530

    created dashboard page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,15 @@
-import Home from './pages/Home';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import ListMovies from './pages/ListMovies';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/listmovies" element={<ListMovies />} />
+        <Route path='/dashboard' element={<Dashboard />} />
     </Routes>
   );
 };
